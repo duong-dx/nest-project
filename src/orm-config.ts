449f039ae263d8f339f32f103e11868e72255a4b
duongdx <dinh.xuan.duong@sun-asterisk.com>
2022-03-16 01:44:04 +0700

    add docker

--- src/orm-config.ts
+++ src/orm-config.ts
@@ -1,12 +1,24 @@
-import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-const config: MysqlConnectionOptions = {
-  type: 'mysql',
-  database: 'nest',
-  username: 'root',
-  password: 'root',
-  port: 3306,
-  host: '127.0.0.1',
+enum DatabaseConnectionEnum {
+  'mysql' = 'mysql',
+  'postgres' = 'postgres',
+}
+
+const type: string[] = ['mysql', 'postgres'];
+
+const databaseConnect: DatabaseConnectionEnum =
+  type.indexOf(process.env.DB_CONNECTION) > -1
+    ? (process.env.DB_CONNECTION as DatabaseConnectionEnum)
+    : DatabaseConnectionEnum.mysql;
+
+const config: TypeOrmModuleOptions = {
+  type: databaseConnect,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+  host: process.env.DB_HOST,
   entities: ['dist/models/**/*.entity{.ts,.js}'],
   synchronize: false,
   dropSchema: false,
